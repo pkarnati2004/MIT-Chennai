5a6af923da97acb3c641dcf6fa38094fe128516f
pkarnati2004 <pkarnati@mit.edu>
2019-01-13 12:05:59 +0530

    flask templates

--- app/app.py
+++ app/app.py
@@ -1,6 +1,15 @@
+import os
+
 from flask import Flask
+from flask import url_for, redirect, render_template, flash, g, session
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+    
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
