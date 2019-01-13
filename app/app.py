f710f8ce2610ab5ce756dbfdb0c3d90cea777299
pkarnati2004 <pkarnati@mit.edu>
2019-01-13 14:03:46 +0530

    add webpage

--- app/app.py
+++ app/app.py
@@ -1,13 +1,23 @@
 import os
 
 from flask import Flask
-from flask import url_for, redirect, render_template, flash, g, session
+from flask import url_for, redirect, render_template, flash, g, session, request
+
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html', name="Pramoda Karnati")
+    return render_template('index.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    gender = request.form['gender']
+    race = request.form['race']
 
     
 if __name__ == '__main__':
